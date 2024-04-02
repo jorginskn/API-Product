a40d74ba4b1ccce711840d28849e535d0dfd29e9
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-04-02 20:55:55 -0300

    Adicioando nova tabela e fazendo a relação da tabela tags com  o produto

--- Migrations/20240402235425_AddTag.Designer.cs
+++ Migrations/20240402235425_AddTag.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace api.net.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240402235425_AddTag")]
+    partial class AddTag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Program.cs
+++ Program.cs
@@ -85,16 +85,31 @@ public class ProductRepository // Removi a palavra-chave static
 
 public class Category {
     public int Id { get; set; }
+
     public string Name { get; set; }
 }
+
+public class Tag {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
 public class Product
 {
     public int Id { get; set; }
+
     public string Code { get; set; }
+
     public string Name { get; set; }
+
     public string Description { get; set; }
+
     public int CategoryId { get; set; }
+
     public Category Category { get; set; }
+    
+    public List<Tag> Tags {get; set;}
 }
 
 public class ApplicationDbContext: DbContext{
